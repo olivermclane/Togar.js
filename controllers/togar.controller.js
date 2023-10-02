43d96db2999a3ec02981b92bcf9408598eb13ba9
Oliver McLane <olivermclane@Thor.local>
2023-10-01 22:45:59 -0600

    Adding functionality to upload images and add them to local storage

--- controllers/togar.controller.js
+++ controllers/togar.controller.js
@@ -1,9 +1,45 @@
+require('../config/multer.config')
+const {upload} = require("../config/multer.config");
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+
 const togarView = (req, res) => {
-    res.render("togar", {
-        user: req.user
+    const directoryPath = path.join(__dirname, '../uploads');
+    // Read files from the uploads directory
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            console.error('Error reading files:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        // Create an array of image URLs
+        const imageUrls = files.map(file => `/uploads/${file}`);
+
+        // Render your view (assuming you're using a templating engine like EJS)
+        res.render('togar', {user: req.user, images: imageUrls });
     });
 }
 
+const togarUploadImage = (req, res) => {
+    console.log(req.file);
+    upload.single('imageFile')(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            // Handle Multer errors (e.g., file size limit exceeded)
+            return res.status(400).json({ message: 'Multer Error: ' + err.message });
+        } else if (err) {
+            // Handle other errors
+            return res.status(500).json({ message: 'Internal Server Error' + err.message});
+        }
+
+        // File uploaded successfully, req.file contains the file details
+        console.log("test"+ req.file);
+        res.redirect("/togar");
+    });
+};
+
 module.exports = {
-    togarView
+    togarView,
+    togarUploadImage
 }
