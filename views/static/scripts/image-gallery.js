af0b295fe0893960e49f5d846b1cddb1e9030875
Oliver McLane <olivermclane@Thor.local>
2023-10-22 14:58:13 -0600

    tg-8:fixed slider navigation was missing slide element

--- views/static/scripts/image-gallery.js
+++ views/static/scripts/image-gallery.js
@@ -1,36 +1,39 @@
-const slider = document.querySelector('.slider');
-const sliderInner = slider.querySelector('.slider-inner');
-const slides = slider.querySelectorAll('.slide');
-const prevBtn = slider.querySelector('.slider-prev');
-const nextBtn = slider.querySelector('.slider-next');
-let slideWidth = slides[0].offsetWidth;
-let slideIndex = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.querySelector('.slider');
+    const sliderInner = slider.querySelector('.slider-inner');
+    const slides = slider.querySelectorAll('.slide');
+    const prevBtn = slider.querySelector('.slider-prev');
+    const nextBtn = slider.querySelector('.slider-next');
+    let slideWidth = 0;
+    let slideIndex = 0;
 
-function slideTo(index) {
-    sliderInner.style.transform = `translateX(-${slideWidth * index}px)`;
-    currentSlideIndex = index;
-}
-
-function slideTo(index) {
-    sliderInner.style.transform = `translateX(-${slideWidth * index}px)`;
-    slideIndex = index;
-}
-
-function prevSlide() {
-    slideIndex--;
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
+    // Check if there are slides before accessing the first slide's offsetWidth
+    if (slides.length > 0) {
+        slideWidth = slides[0].offsetWidth;
+        console.log(slideWidth); // Log slideWidth to check if it has a valid value
     }
-    slideTo(slideIndex);
-}
 
-function nextSlide() {
-    slideIndex++;
-    if (slideIndex >= slides.length) {
-        slideIndex = 0;
+    function slideTo(index) {
+        sliderInner.style.transform = `translateX(-${slideWidth * index}px)`;
+        slideIndex = index;
     }
-    slideTo(slideIndex);
-}
 
-prevBtn.addEventListener('click', prevSlide);
-nextBtn.addEventListener('click', nextSlide);
+    function prevSlide() {
+        slideIndex--;
+        if (slideIndex < 0) {
+            slideIndex = slides.length - 1;
+        }
+        slideTo(slideIndex);
+    }
+
+    function nextSlide() {
+        slideIndex++;
+        if (slideIndex >= slides.length) {
+            slideIndex = 0;
+        }
+        slideTo(slideIndex);
+    }
+
+    prevBtn.addEventListener('click', prevSlide);
+    nextBtn.addEventListener('click', nextSlide);
+});
